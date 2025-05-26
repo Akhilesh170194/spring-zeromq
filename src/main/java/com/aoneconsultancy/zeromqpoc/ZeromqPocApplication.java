46feea23beca1e7208f5f0a55c29fbffc12fe5ca
Akhilesh Singh <akhilesh.singh@haud.com>
2025-05-26 16:12:33 +0530

    Initial commit

---
+++ src/main/java/com/aoneconsultancy/zeromqpoc/ZeromqPocApplication.java
@@ -0,0 +1,13 @@
+package com.aoneconsultancy.zeromqpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeromqPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeromqPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aoneconsultancy/zeromqpoc/ZeromqPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aoneconsultancy.zeromqpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeromqPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
